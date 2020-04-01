1e14c660ee48449aec98635c69a67006b5d4bf02
Mitchel Kuijpers <mitchelkuijpers@gmail.com>
2020-04-01 15:45:58 +0200

    add checkbox icons and data fetch helpers

--- src/fulcro_atlaskit/utils.cljs
+++ src/fulcro_atlaskit/utils.cljs
@@ -2,7 +2,8 @@
   (:require
     [goog.object :as gobj]
     [com.fulcrologic.guardrails.core :refer [>defn =>]]
-    [cljs.spec.alpha :as s]))
+    [cljs.spec.alpha :as s]
+    [com.fulcrologic.fulcro.data-fetch :as df]))
 
 (>defn js-spread
   "This works like the javascript spread operator if you see examples like: {... props}"
@@ -19,3 +20,15 @@
 (defn vec-move "Move elem in coll by index"
   [coll pos1 pos2]
   (let [el (nth coll pos1)] (if (= pos1 pos2) coll (into [] (vec-add (vec-remove coll pos1) pos2 el)))))
+
+(defn loading?
+  ([props marker-kw]
+   (loading? (get props [df/marker-table marker-kw])))
+  ([marker]
+   (or
+     (df/ready? marker)
+     (df/loading? marker))))
+
+(def done? (complement loading?))
+
+(def failed? df/failed?)
