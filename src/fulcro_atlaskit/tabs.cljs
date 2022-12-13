a24a739ebfab0cd72383c94ee5479bb625600af7
Rambabu Patina <rambabu.patina@appfire.com>
2022-12-13 15:51:30 +0530

    NH-51: Adding tabs other comps

--- src/fulcro_atlaskit/tabs.cljs
+++ src/fulcro_atlaskit/tabs.cljs
@@ -1,6 +1,9 @@
 (ns fulcro-atlaskit.tabs
   (:require
-    ["@atlaskit/tabs" :as atlaskit-tabs]
+    ["@atlaskit/tabs" :as atlaskit-tabs :refer [Tab TabList TabPanel]]
     [com.fulcrologic.fulcro.algorithms.react-interop :as interop]))
 
 (def ui-tabs (interop/react-factory (.-default atlaskit-tabs)))
+(def ui-tab (interop/react-factory Tab))
+(def ui-tab-list (interop/react-factory TabList))
+(def ui-tab-panel (interop/react-factory TabPanel))
