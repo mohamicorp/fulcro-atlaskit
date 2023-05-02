278b7ed2603a67669edd7d2ad9c832f2411ec479
Rambabu Patina <rambabu.patina@appfire.com>
2023-05-02 17:13:52 +0530

    NH-193: Specific import

--- src/fulcro_atlaskit/fuegokit.cljs
+++ src/fulcro_atlaskit/fuegokit.cljs
@@ -1,6 +1,7 @@
 (ns fulcro-atlaskit.fuegokit
   (:require
-   ["@fuegokit/react" :refer (Box GlobalHeader)]
+   ;;["@fuegokit/react" :refer (Box)]
+   ["@fuegokit/react/lib-esm/Box$default" :as Box]
    [com.fulcrologic.fulcro.algorithms.react-interop :as react-interop]))
 
 ;;(def ui-fuegokit (react-interop/react-factory fuegokit))
