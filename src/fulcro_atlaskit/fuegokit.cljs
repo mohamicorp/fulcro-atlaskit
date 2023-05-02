0e03197dc8f2016debd1fae9df77ded31dd0cbde
Rambabu Patina <rambabu.patina@appfire.com>
2023-05-02 15:54:28 +0530

    NH-193: Default import

--- src/fulcro_atlaskit/fuegokit.cljs
+++ src/fulcro_atlaskit/fuegokit.cljs
@@ -1,8 +1,8 @@
 (ns fulcro-atlaskit.fuegokit
   (:require
-   ["@fuegokit/react$default" :as fuegokit]
+   ["@fuegokit/react$default" :as appfire-fuegokit]
    [com.fulcrologic.fulcro.algorithms.react-interop :as react-interop]))
 
-(def ui-fuegokit (react-interop/react-factory fuegokit))
-;;(def ui-box (react-interop/react-factory Box))
-;;(def ui-global-header (react-interop/react-factory GlobalHeader))
+;;(def ui-fuegokit (react-interop/react-factory fuegokit))
+(def ui-box (react-interop/react-factory appfire-fuegokit/Box))
+(def ui-global-header (react-interop/react-factory appfire-fuegokit/GlobalHeader))
