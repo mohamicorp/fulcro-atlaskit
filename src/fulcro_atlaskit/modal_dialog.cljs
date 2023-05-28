26e014e503ff9ac4b30ae8c57c4a8c92e1ba2fb4
Rambabu Patina <rambabu.patina@appfire.com>
2023-05-28 15:16:20 +0530

    NH-157: Adding modal title and body

--- src/fulcro_atlaskit/modal_dialog.cljs
+++ src/fulcro_atlaskit/modal_dialog.cljs
@@ -1,9 +1,11 @@
 (ns fulcro-atlaskit.modal-dialog
   (:require
-    ["@atlaskit/modal-dialog" :default Modal :refer [ModalTransition ModalFooter ModalHeader]]
+    ["@atlaskit/modal-dialog" :default Modal :refer [ModalTransition ModalFooter ModalHeader ModalBody ModalTitle]]
     [com.fulcrologic.fulcro.algorithms.react-interop :as react-interop]))
 
 (def ui-modal (react-interop/react-factory Modal))
 (def ui-modal-footer (react-interop/react-factory ModalFooter))
 (def ui-modal-header (react-interop/react-factory ModalHeader))
+(def ui-modal-title (react-interop/react-factory ModalTitle))
+(def ui-modal-body (react-interop/react-factory ModalBody))
 (def ui-modal-transition (react-interop/react-factory ModalTransition))
